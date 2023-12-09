8a6d9d3c8e601977c13d06dcc9ee5002e71c83f3
mxnuchim <manuchimoliver779@gmail.com>
2023-12-09 14:05:30 +0100

    feat: login & si

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -1,40 +1,88 @@
+import axios from 'axios';
 import { useState } from 'react';
+import { BASE_URL } from '../../data';
+import AnimationView from './AnimationView';
 
 interface AuthFormProps {
   onClose: () => void;
   type: 'login' | 'signup';
-  onLogin?: () => void;
-  onSignup?: () => void;
 }
 
-const AuthForm: React.FC<AuthFormProps> = ({
-  onClose,
-  type,
-  onLogin,
-  onSignup,
-}) => {
+const AuthForm: React.FC<AuthFormProps> = ({ onClose, type }) => {
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  const handleLogin = () => {
-    // Handle login logic here
-    console.log('Login:', email, password);
-    if (!email || !password) return;
-
-    if (onLogin) {
-      onLogin();
+  const handleLogin = async () => {
+    if (!email || !password) {
+      console.log('missing values');
+      alert('please enter the required values');
+      return;
     }
+    console.log(
+      `calling login at ${BASE_URL + '/user/login'} with data --> `,
+      email,
+      password
+    );
+    setLoading(true);
+
+    const { data } = await axios.post(
+      BASE_URL + '/user/login',
+      {
+        email: email,
+        password: password,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    console.log('response --> ', data);
+    if (data.status === 200) {
+      alert("You've logged in!");
+      sessionStorage.setItem('user', JSON.stringify(data.data));
+    }
+    setLoading(false);
+    onClose();
+
     onClose();
   };
 
-  const handleSignup = () => {
-    // Handle signup logic here
-    console.log('Signup:', name, email, password);
-    if (!name || !email || !password) return;
-    if (onSignup) {
-      onSignup();
+  const handleSignup = async () => {
+    // console.log(
+    //   `calling signup at ${BASE_URL + '/user/signup'} with data --> `,
+    //   name,
+    //   email,
+    //   password
+    // );
+    if (!name || !email || !password) {
+      console.log('missing values');
+      alert('please enter the required values');
+      return;
     }
+    setLoading(true);
+
+    const { data } = await axios.post(
+      BASE_URL + '/user/signup',
+      {
+        name: name,
+        email: email,
+        password: password,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    console.log('response --> ', data);
+    if (data.status === 200) {
+      alert("You've signed up!");
+      sessionStorage.setItem('user', JSON.stringify(data.data));
+    }
+    setLoading(false);
     onClose();
   };
 
@@ -43,108 +91,112 @@ const AuthForm: React.FC<AuthFormProps> = ({
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 z-50">
-      <div className="bg-white p-6 rounded-md w-96">
-        <h2 className="text-2xl font-semibold mb-4">Login or Signup</h2>
+      {loading ? (
+        <AnimationView animationType="loading" message="Signing up..." />
+      ) : (
+        <div className="bg-white p-6 rounded-md w-96">
+          <h2 className="text-2xl font-semibold mb-4">Login or Signup</h2>
 
-        {isLogin ? (
-          <>
-            {/* Login Form */}
-            <div className="mb-4">
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-600"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                id="login-email"
-                className="mt-1 p-2 w-full border rounded-md"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="login-password"
-                className="block text-sm font-medium text-gray-600"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                id="login-password"
-                className="mt-1 p-2 w-full border rounded-md"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-          </>
-        ) : null}
+          {isLogin ? (
+            <>
+              {/* Login Form */}
+              <div className="mb-4">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-600"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="login-email"
+                  className="mt-1 p-2 w-full border rounded-md"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="login-password"
+                  className="block text-sm font-medium text-gray-600"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="login-password"
+                  className="mt-1 p-2 w-full border rounded-md"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </>
+          ) : null}
 
-        {isSignup ? (
-          <>
-            {/* Signup Form */}
-            <div className="mt-6">
-              <label
-                htmlFor="signup-name"
-                className="block text-sm font-medium text-gray-600"
-              >
-                Name
-              </label>
-              <input
-                type="text"
-                id="signup-name"
-                className="mt-1 p-2 w-full border rounded-md"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="signup-email"
-                className="block text-sm font-medium text-gray-600"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                id="signup-email"
-                className="mt-1 p-2 w-full border rounded-md"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="signup-password"
-                className="block text-sm font-medium text-gray-600"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                id="signup-password"
-                className="mt-1 p-2 w-full border rounded-md"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-          </>
-        ) : null}
-        <div className="w-full flex flex-row items-center justify-between py-2">
-          <button
-            className="bg-accent text-white py-2 px-4 rounded-md"
-            onClick={isLogin ? handleLogin : handleSignup}
-          >
-            {isLogin ? 'Login' : 'Signup'}
-          </button>
+          {isSignup ? (
+            <>
+              {/* Signup Form */}
+              <div className="mt-6">
+                <label
+                  htmlFor="signup-name"
+                  className="block text-sm font-medium text-gray-600"
+                >
+                  Name
+                </label>
+                <input
+                  type="text"
+                  id="signup-name"
+                  className="mt-1 p-2 w-full border rounded-md"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="signup-email"
+                  className="block text-sm font-medium text-gray-600"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="signup-email"
+                  className="mt-1 p-2 w-full border rounded-md"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="signup-password"
+                  className="block text-sm font-medium text-gray-600"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="signup-password"
+                  className="mt-1 p-2 w-full border rounded-md"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </>
+          ) : null}
+          <div className="w-full flex flex-row items-center justify-between py-2">
+            <button
+              className="bg-accent text-white py-2 px-4 rounded-md"
+              onClick={isLogin ? handleLogin : handleSignup}
+            >
+              {isLogin ? 'Login' : 'Signup'}
+            </button>
 
-          <div className="cursor-pointer" onClick={onClose}>
-            <p className="text-base">Close</p>
+            <div className="cursor-pointer" onClick={onClose}>
+              <p className="text-base">Close</p>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
--- src/components/HeaderMain.tsx
+++ src/components/HeaderMain.tsx
@@ -1,15 +1,21 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { BsSearch } from 'react-icons/bs';
 import { BiUser } from 'react-icons/bi';
 import { FiHeart } from 'react-icons/fi';
 import { HiOutlineShoppingBag } from 'react-icons/hi';
 import AuthForm from './AuthForm';
+import axios from 'axios';
 
 const HeaderMain = () => {
   const [registerModalOpen, setRegisterModalOpen] = useState<boolean>(false);
   const [loginModalOpen, setLoginModalOpen] = useState<boolean>(false);
+  const [userData, setUserData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
 
   const toggleLoginModal = () => {
     setLoginModalOpen(!loginModalOpen);
@@ -18,6 +24,22 @@ const HeaderMain = () => {
     setRegisterModalOpen(!registerModalOpen);
   };
 
+  const getUserData = () => {
+    const storedUserDataString = sessionStorage.getItem('user');
+
+    if (storedUserDataString !== null) {
+      const storedUserData = JSON.parse(storedUserDataString);
+      setUserData(storedUserData);
+      console.log('User data from sessionStorage:', storedUserData);
+    } else {
+      console.log('No user data found in sessionStorage');
+    }
+  };
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
   return (
     <div className="border-b border-gray-200 py-6">
       <div className="container sm:flex justify-between items-center">
@@ -25,18 +47,31 @@ const HeaderMain = () => {
           SickBooks
         </div>
 
-        <div className="hidden lg:flex gap-4 text-gray-500 text-[30px]">
-          <button className="bg-accent rounded-full" onClick={toggleLoginModal}>
-            <p className="text-white text-base text-center px-3 py-1">Login</p>
-          </button>
-          <button
-            className="bg-accent rounded-full"
-            onClick={toggleRegisterModal}
-          >
-            <p className="text-white text-base text-center px-3 py-1">
-              Register
+        <div className="flex gap-4 text-gray-500 text-[30px] justify-center items-center sm:justify-between">
+          {userData ? (
+            <p className="text-base mg:text-lg text-blackish font-semibold text-end">
+              {userData.name}
             </p>
-          </button>
+          ) : (
+            <div className="flex flex-row gap-3">
+              <button
+                className="bg-blackish rounded-full"
+                onClick={toggleLoginModal}
+              >
+                <p className="text-white text-base text-center px-3 py-1">
+                  Login
+                </p>
+              </button>
+              <button
+                className="bg-blackish rounded-full"
+                onClick={toggleRegisterModal}
+              >
+                <p className="text-white text-base text-center px-3 py-1">
+                  Register
+                </p>
+              </button>
+            </div>
+          )}
           <BiUser />
         </div>
       </div>
