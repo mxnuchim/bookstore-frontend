cff64bd16616c7515b4c2fa3546f7d5c07b26a9f
mxnuchim <manuchimoliver779@gmail.com>
2023-12-11 10:00:31 +0100

    https fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </head>
       <body className={inter.className}>
         <HeaderTop />
         <HeaderMain />
